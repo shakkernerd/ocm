6f1aa1e8985524e7b1357c91c069b2b147f67a09
Shakker <shakkerdroid@gmail.com>
2026-03-26 15:00:00 +0000

    refactor: route cli launcher access through launcher module

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -9,7 +9,8 @@ use std::path::PathBuf;
 
 use serde::Serialize;
 
-use crate::services::{EnvironmentService, LauncherService, RuntimeService};
+use crate::launcher::LauncherService;
+use crate::services::{EnvironmentService, RuntimeService};
 use crate::store::ensure_store;
 
 const VERSION: &str = "0.1.0";
