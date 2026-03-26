b421812578ef6b441b01f3ed58c050a9f5a7ea9c
Shakker <shakkerdroid@gmail.com>
2026-03-26 21:40:00 +0000

    refactor: move store path layout into store module

--- src/store.rs
+++ src/store.rs
@@ -1,5 +1,6 @@
 mod common;
 mod environments;
+mod layout;
 mod launchers;
 mod prune;
 mod runtimes;
@@ -10,9 +11,14 @@ use std::path::Path;
 
 use time::OffsetDateTime;
 
-use crate::paths::{derive_env_paths, display_path, resolve_store_paths};
 use crate::types::{EnvMeta, EnvSummary, StorePaths};
 use common::ensure_dir;
+pub use layout::{
+    clean_path, default_env_root, derive_env_paths, display_path, env_meta_path,
+    launcher_meta_path, resolve_absolute_path, resolve_ocm_home, resolve_store_paths,
+    resolve_user_home, runtime_install_files_dir, runtime_install_root, runtime_meta_path,
+    snapshot_archive_path, snapshot_env_dir, snapshot_meta_path, validate_name,
+};
 pub use environments::{
     clone_environment, create_environment, export_environment, get_environment,
     import_environment, list_environments, remove_environment, repair_environment_marker,
