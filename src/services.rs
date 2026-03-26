b38217453549dd2e0a0f06e9e856ee30401b6003
Shakker <shakkerdroid@gmail.com>
2026-03-26 15:20:00 +0000

    refactor: drop launcher service shim from services

--- src/services.rs
+++ src/services.rs
@@ -1,7 +1,5 @@
 mod environments;
-mod launchers;
 mod runtimes;
 
 pub use environments::{EnvironmentService, ResolvedExecution};
-pub use launchers::LauncherService;
 pub use runtimes::RuntimeService;
